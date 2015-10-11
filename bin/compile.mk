5d23e84f39d344674cb6ba4bf4cd6c8723e27a71
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2015-10-12 04:36:06 +0900

    Rollback pre-build to self-compile

--- bin/compile.mk
+++ bin/compile.mk
@@ -3,14 +3,12 @@
 # Usage:
 #   $ make -f compile.mk BUILD_DIR=$1
 
-VERSION=24.5
+VERSION=24.3
 EMACS_SOURCE_URL=http://ftpmirror.gnu.org/emacs/emacs-${VERSION}.tar.xz
 
 VENDOR_DIR=/app/.heroku/vendor
 
 EMACS_PACKAGE=${BUILD_DIR}/emacs-${VERSION}-heroku-bin.tar.xz
-
-default: ${EMACS_PACKAGE}
 
 ${EMACS_PACKAGE}:
 	curl -L --silent ${EMACS_SOURCE_URL} | tar xJm --strip-components=1
@@ -19,8 +17,12 @@ ${EMACS_PACKAGE}:
 	make install-strip
 	tar cJf ${EMACS_PACKAGE} -C ${VENDOR_DIR} .
 
-setup: ${EMACS_PACKAGE}
+# build package
+build: ${EMACS_PACKAGE}
+
+# install package
+install: ${EMACS_PACKAGE}
 	mkdir -p ${BUILD_DIR}/.heroku/vendor
 	tar xJf ${EMACS_PACKAGE} -C ${BUILD_DIR}/.heroku/vendor
 
-.PHONY: setup
+.PHONY: build install
